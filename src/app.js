c1319ba9c06bb160f9bf1064ffda4837011e3b47
Lakshya Dosi <lakshya.dosi@intimetec.com>
2024-11-19 10:23:52 +0530

    create the server

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.use("/test",(req,res)=>{
+    res.send("hello from the server");
+})
+
+app.listen(3000, ()=>{
+    console.log("server started")
+})
